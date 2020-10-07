cfdce4092505b1d13a13872b2b8cfbede77ebe59
yuvraj Pawar <yurajpawar99@gmail.com>
2020-10-07 15:53:41 +0530

    minor commit

--- public/db.js
+++ public/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const dotenv=require("dotenv");
 const connection=()=>{
     
-mongoose.connect("mongodb+srv://"+(process.env.DATABASE_USERNAME)+":"+(process.env.DATABASE_PASSWORD)+"@autodoggDb.ia2jk.mongodb.net/AutoDogg?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://"+(process.env.DATABASE_USERNAME)+":"+(process.env.DATABASE_PASSWORD)+"@autodoggDb.ia2jk.mongodb.net/AutoDogg",
      {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true
+   
 },(err)=>{
     if(!err){
         console.log("Connected to the database!");
@@ -18,4 +18,4 @@ mongoose.set('useFindAndModify', false);
 
 module.exports=connection;
 
-//mongoose.connect("mongodb://localhost:27017/AutoDogg",
+//mongoose.connect("mongodb://localhost:27017/AutoDogg",  useUnifiedTopology: true
